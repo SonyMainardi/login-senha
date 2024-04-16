6c19d06b90434277160957feb592c95df8861640
p_921469 <sony.silva@camara.leg.br>
2024-04-16 10:49:02 -0300

    primeiro e unico commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #51259D;
+    color: #FFF;
+
+    border: 1px solid #51259D;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
+import { defaultValues, IFormLogin } from "./types";
+
+const schema = yup
+  .object({
+    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(6, "No mínimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
+
+const Login = () => {
+  const {
+    control,
+    formState: { errors, isValid },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    defaultValues,
+    reValidateMode: "onChange",
+  });
+
+  // Use um estado para controlar a visibilidade do botão
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+  // UseEffect para atualizar a visibilidade do botão com base na validade
+  useEffect(() => {
+    setIsButtonVisible(isValid);
+  }, [isValid]);
+
+  return (
+    <Container>
+      <LoginContainer>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <Input
+            name="email"
+            placeholder="Email"
+            control={control}
+            errorMessage={errors?.email?.message}
+          />
+          <Spacing />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            control={control}
+            errorMessage={errors?.password?.message}
+          />
+          <Spacing />
+          {/* Renderize o botão apenas se a validade for verdadeira */}
+          {isButtonVisible && <Button title="ENTRAR" />}
+        </Column>
+      </LoginContainer>
+    </Container>
+  );
+};
+
+export default Login;
